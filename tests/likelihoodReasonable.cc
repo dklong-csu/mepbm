3dc0fd0a7e57c0fe66b6f64db2107725fbdc8d00
Danny Long <dannylong6794@gmail.com>
2020-09-08 15:45:24 -0600

    Removed comments of previous code.

--- tests/likelihoodReasonable.cc
+++ tests/likelihoodReasonable.cc
@@ -10,21 +10,6 @@
 
 int main()
 {
-  // S3 data
-  /*
-  const std::valarray<double> dataDiam = {2.98, 2.82, 1.84, 2.04, 1.56, 1.56,
-                                          1.2, 1.18, 2.06, 2.27, 2.54, 2.09,
-                                          1.63, 1.91, 1.96, 2.09, 2.39, 2.17,
-                                          1.98, 1.69, 2.47, 1.87, 2.03, 1.5,
-                                          2.73, 1.65, 2.05, 2.21, 2.38, 3.07,
-                                          2.93, 2.67, 3.83, 2.95, 3.3, 2.82,
-                                          2.49, 2.62, 2.09, 3.02, 2.94, 3.25,
-                                          2.43, 1.92, 3.22, 2.86, 2.74, 3.09,
-                                          3.19, 1.73, 1.94, 2.14, 2.91, 2.85,
-                                          2.8, 2.37, 2.42, 2.68, 2.01, 1.9,
-                                          2.14};
-*/
-
   const Data::PomData pom_data;
   const std::valarray<double> dataDiam = pom_data.tem_diam_time2;
 
@@ -63,12 +48,6 @@ int main()
 
   auto binned_data = std::get<1>(logLikeliResults);
 
-  //Histograms::Histogram dataHistogram(histogramParameters);
-
-  //std::valarray<double> dataCounts(1.0, fitData[0].size());
-
-  //dataHistogram.AddToBins(dataCounts, fitData[0]);
-
   std::cout << "Bin counts for data: "
             << std::endl;
 
@@ -79,34 +58,6 @@ int main()
   }
 
   // print probability mass function
-
-  // Step 1 - solve the ODE
-   //const std::vector<std::valarray<double>> particleSizeDistr = Models::integrate_ode_explicit_euler(solverParameters, model, prm, time_step);
-  // Step 2 - convert ODE solution to probability distribution on relevant particle sizes (i.e. sizes nucleationOrder - maxSize)
-
-  // identify smallest and largest particle sizes
-  //const unsigned int smallestParticle = model.getSmallestParticleSize(prm);
-  //const unsigned int largestParticle = model.getLargestParticleSize(prm);
-
-  // isolate the relevant particle sizes
-  //std::valarray<double> particleSizes (largestParticle - smallestParticle + 1);
-  //std::valarray<double> concentrations (particleSizes.size());
-  //for (unsigned int size = smallestParticle; size < largestParticle + 1; size++)
-  {
-    // vector holding particle sizes
-    //particleSizes[size - smallestParticle] = size;
-    // vector holding corresponding concentrations
-    //concentrations[size - smallestParticle] = particleSizeDistr[0][model.particleSizeToIndex(size, prm)];
-  }
-
-  // normalize subset vector so its sum is equal to 1
-  //const double normalizeConst = concentrations.sum();
-  //const std::valarray<double> particleSizePMF = concentrations/normalizeConst;
-
-  // create histogram from PMF to aggregate bin probability
-  //Histograms::Histogram odeHistogram(histogramParameters);
-
-  //odeHistogram.AddToBins(particleSizePMF, particleSizes);
 
   auto binned_pmf = std::get<2>(logLikeliResults);
 
