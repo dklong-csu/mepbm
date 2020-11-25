b67eb61631c9dd22ba50598b4211766c6eb3081d
Danny <dannylongcsu@gmail.com>
2020-11-24 22:30:30 -0700

    Updated the statistics functions to reflect the change to std::vector and the new ODE methodology.

--- tests/log_likelihood_basic.cc
+++ tests/log_likelihood_basic.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <valarray>
+#include <boost/numeric/odeint.hpp>
 #include "models.h"
 #include "histogram.h"
 #include "statistics.h"
@@ -10,11 +10,11 @@
 int main()
 {
   // create dummy data
-  std::valarray<double> data = {3,4,5,6,4,5};
+  std::vector<double> data = {3,4,5,6,4,5};
 
   // create dummy distribution
-  std::valarray<double> sizes = {3, 4, 5, 6};
-  std::valarray<double> distr = {1, 2, 3, 4};
+  std::vector<double> sizes = {3, 4, 5, 6};
+  std::vector<double> distr = {1, 2, 3, 4};
 
   // create histogram parameters
   Histograms::Parameters hist_prm(4, 3, 6);
