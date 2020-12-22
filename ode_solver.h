3be3dc1c1ffbe86e80392369e3db2b7a0bee60da
Danny <dannylongcsu@gmail.com>
2020-12-22 16:55:56 -0700

    Add function for Newton's method and corresponding test.

--- ode_solver.h
+++ ode_solver.h
@@ -1,12 +1,15 @@
 #ifndef MEPBM_ODE_SOLVER_H
 #define MEPBM_ODE_SOLVER_H
 
+#include <Eigen/Dense>
+#include <cmath>
+
 
 namespace ODE
 {
   /*
    * A function which solves an ODE in an abstract way. The details of how to proceed from one time step to the next
-   * is left up to the input.
+   * is left up to the input ODE system using its step_forward member function.
    */
   template<class OdeSystemType, class VectorType>
   VectorType solve_ode(const OdeSystemType &ode_system, const VectorType &ic,
@@ -42,6 +45,51 @@ namespace ODE
 
     return x1;
   }
+
+
+
+  /*
+   * A function which performs Newton's method to find the root of a nonlinear equation.
+   * Technically, this is a modification where the Jacobian is taken as a constant for the
+   * entire process. This is to save time as computing a decomposition of a Jacobian is more
+   * expensive than solving with a precomputed decomposition (e.g. LU decomposition).
+   */
+  template<class FunctionType, class VectorType>
+  VectorType perform_newtons_method(const FunctionType &function, const VectorType &guess,
+                                    const double tol = 1e-6, const unsigned int max_iter = 100)
+  {
+    bool solution_not_found = true;
+    unsigned int iter = 0;
+
+    auto x0 = guess;
+    auto x1 = guess;
+    while (solution_not_found && iter < max_iter)
+    {
+      auto f = function.value(x0);
+      // J(x_{n+1} - x_n) = -f
+      auto d = function.jacobian.solve(-f);
+      x1 = x0 + d;
+
+      // Check how close to zero the Newton function is after this iteration.
+      // Divide the l2 norm of the Newton function by the minimum l2 norm of x0 and x1
+      // in order to solve to an accuracy appropriate to the units of x.
+      f = function.value(x1);
+      auto diff = f / std::min(x0.norm(), x1.norm());
+      if (diff.norm() < tol)
+        solution_not_found = true;
+
+      // increment the iteration counter
+      ++iter;
+      // x1 we just solved for becomes the new guess
+      x0 = x1;
+    }
+    return x1;
+  }
+
+
+
+
+
 }
 
 #endif //MEPBM_ODE_SOLVER_H
