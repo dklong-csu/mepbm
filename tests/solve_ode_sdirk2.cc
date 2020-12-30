c7ed6b80cfec4ee1eeb6fdbfe32c4e00b4ab52c1
Danny <dannylongcsu@gmail.com>
2020-12-30 14:47:05 -0700

    Test for SDIRK method order 1.

---
+++ tests/solve_ode_sdirk2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <ode_solver.h>
+#include <eigen3/Eigen/Dense>
+
+int main()
+{
+  const Eigen::VectorXd ic = Eigen::VectorXd::Ones(1);
+  const double start_time = 0.;
+  const double end_time = 1.;
+  const double dt = 1e-4;
+
+  // FIXME: this will require changes once OdeSystem is updated
+  ODE::OdeSystem ode_system;
+  ODE::StepperSDIRK<2> stepper(ode_system);
+  auto sol = ODE::solve_ode(stepper, ic, start_time, end_time, dt);
+
+  // The answer should be close to exp(-10)
+  std::cout << sol;
+}
