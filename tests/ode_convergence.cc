622a45ffe9dd5e8efd60ac5856b4be74a55a737b
Danny <dannylongcsu@gmail.com>
2021-01-07 11:29:00 -0700

    I just want to see what happens when GitHub runs this test.

--- tests/ode_convergence.cc
+++ tests/ode_convergence.cc
@@ -23,7 +23,7 @@ class SimpleOde : public Model::RightHandSideContribution
 
 int main()
 {
-  std::vector<double> dt_vals = { 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7};
+  std::vector<double> dt_vals = { 1e-1, 1e-2, 1e-3, 1e-4, 1e-5};
   const int digits = 100;
   
 
