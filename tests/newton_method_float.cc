bf0598a9fb93e0d03c96e9cf17d5c0e9ec5a9a11
Danny <dannylongcsu@gmail.com>
2021-03-04 23:31:09 -0700

    Testing for double and float.

---
+++ tests/newton_method_float.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "ode_solver.h"
+#include <eigen3/Eigen/Dense>
+
+
+
+using Real = float;
+
+
+
+class Fcn : public ODE::FunctionBase<Real>
+{
+public:
+  Eigen::Matrix<Real, Eigen::Dynamic, 1> value(const Eigen::Matrix<Real, Eigen::Dynamic, 1> &x) const override;
+};
+
+
+Eigen::Matrix<Real, Eigen::Dynamic, 1> Fcn::value(const Eigen::Matrix<Real, Eigen::Dynamic, 1> &x) const
+{
+  Eigen::Matrix<Real, Eigen::Dynamic, 1> sol(2);
+  sol(0) = x(0) * x(0);
+  sol(1) = x(1) * x(1);
+  return sol;
+}
+
+
+
+int main()
+{
+  Eigen::Matrix<Real, Eigen::Dynamic, 1> guess(2);
+  guess(0) = 3/4;
+  guess(1) = 1/2;
+
+  Eigen::Matrix<Real, Eigen::Dynamic, Eigen::Dynamic> J(2,2);
+  J(0, 0) = 3/2;
+  J(0, 1) = 0;
+  J(1, 0) = 0;
+  J(1, 1) = 1;
+
+  auto J_inverse = J.partialPivLu();
+
+  Fcn fcn;
+
+  auto newton_result = ODE::newton_method<Real>(fcn, J_inverse, guess, 1e-10, 1000);
+
+  // The answer should be close to [0, 0].
+  std::cout << newton_result.first;
+}
---
+++ tests/solve_ode_bdf1_float.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+#include <ode_solver.h>
+#include "models.h"
+#include <eigen3/Eigen/Dense>
+
+
+using Real = float;
+
+class SimpleOde : public Model::RightHandSideContribution<Real>
+{
+  void add_contribution_to_rhs(const Eigen::Matrix<Real, Eigen::Dynamic, 1> &x, Eigen::Matrix<Real, Eigen::Dynamic, 1> &rhs)
+  {
+    rhs += -10 * x;
+  }
+
+  void add_contribution_to_jacobian(const Eigen::Matrix<Real, Eigen::Dynamic, 1> &x, Eigen::Matrix<Real, Eigen::Dynamic, Eigen::Dynamic> &jacobi)
+  {
+    jacobi += -10 * Eigen::Matrix<Real, Eigen::Dynamic, Eigen::Dynamic>::Identity(x.rows(), x.rows());
+  }
+};
+
+int main()
+{
+  const Eigen::Matrix<Real, Eigen::Dynamic, 1> ic = Eigen::Matrix<Real, Eigen::Dynamic, 1>::Ones(1);
+  const Real start_time = 0.;
+  const Real end_time = 1.;
+  const Real dt = 1e-5;
+
+  std::shared_ptr<Model::RightHandSideContribution<Real>> my_ode
+      = std::make_shared<SimpleOde>();
+  Model::Model<Real> ode_system(0, 0);
+  ode_system.add_rhs_contribution(my_ode);
+  ODE::StepperBDF<1, Real> stepper(ode_system);
+  auto sol = ODE::solve_ode<Real>(stepper, ic, start_time, end_time, dt);
+
+  // The answer should be exactly the same as in the solve_ode_sdirk1 test.
+  std::cout << std::setprecision(20) << std::fixed << sol;
+}
