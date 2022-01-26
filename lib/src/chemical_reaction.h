def684f00a67c2ecf6d1b03e9943651d09a67ecb
Danny <dannylongcsu@gmail.com>
2022-01-26 10:06:53 -0700

    Need to construct sparse Jacobian differently than dense to be efficient.

--- lib/src/chemical_reaction.h
+++ lib/src/chemical_reaction.h
@@ -14,6 +14,11 @@
 
 
 namespace MEPBM {
+  /**
+   * An object that contains instructions for the right-hand side and Jacobian of the set of
+   * ordinary differentials equations that arise from modeling a chemical reaction with the
+   * law of mass action.
+   */
   template<typename Real, typename Matrix>
   class ChemicalReaction {
   public:
@@ -144,6 +149,318 @@ namespace MEPBM {
             assert(row >= 0);
             // Products have positive derivatives.
             J->coeffRef(row,col) += chemical.second * deriv;
+          }
+        }
+
+        int success = 0;
+        return success;
+      };
+      return fcn;
+    }
+
+  };
+
+
+
+  /**
+   * An object that contains instructions for the right-hand side and Jacobian of the set of
+   * ordinary differentials equations that arise from modeling a chemical reaction with the
+   * law of mass action. If using a sparse matrix, the formation of the Jacobian needs to be
+   * handled differently to achieve performance, so a partial specialization is used. This
+   * specialization is for compressed column storage.
+   */
+  template<typename Real>
+  class ChemicalReaction<Real, Eigen::SparseMatrix<Real>> {
+  public:
+    /// Constructor.
+    ChemicalReaction(const std::vector< std::pair<Species, unsigned int> > reactants,
+                     const std::vector< std::pair<Species, unsigned int> > products,
+                     const Real rate)
+        : reactants(reactants),
+          products(products),
+          rate(rate)
+    {}
+
+    const std::vector< std::pair<Species, unsigned int> > reactants; /// The species and multiplicity of each reactant.
+    const std::vector< std::pair<Species, unsigned int> > products;  /// The species and multiplicity of each product.
+    const Real rate; /// The reaction rate.
+
+    /// Returns a function for the right-hand side of the ODE that is compatible with the SUNDIALS API.
+    std::function<int(Real, N_Vector, N_Vector, void*)>
+    rhs_function() const {
+      auto fcn = [&](Real time, N_Vector x, N_Vector x_dot, void * user_data)
+      {
+        // We always use N_Vectors based on the Eigen library, so use that to work with the vector.
+        auto x_vec = static_cast<Eigen::Matrix<Real, Eigen::Dynamic, 1>*>(x->content);
+        auto x_dot_vec = static_cast<Eigen::Matrix<Real, Eigen::Dynamic, 1>*>(x_dot->content);
+
+        // The right-hand side contribution from this particular reaction is based on the law of mass action.
+        //    product( reactant_i ^ multiplicity_i ) * rate
+        // Then involved reactant and product is scaled by its multiplicity.
+        Real rxn = rate;
+        for (const auto & r : reactants)
+        {
+          auto index = r.first.index;
+          assert(index >= 0); // make sure nothing is weird with the index
+          assert(index < x_vec->size()); // make sure nothing is weird with the index
+          assert(index < x_dot_vec->size()); // make sure nothing is weird with the index
+          auto exponent = r.second;
+          auto concentration = (*x_vec)(index);
+          rxn *= std::pow(concentration, exponent);
+        }
+
+        // Now distribute the reaction term to all relevant locations in the vector.
+        for (const auto & r : reactants)
+        {
+          auto index = r.first.index;
+          assert(index >= 0); // make sure nothing is weird with the index
+          assert(index < x_vec->size()); // make sure nothing is weird with the index
+          assert(index < x_dot_vec->size()); // make sure nothing is weird with the index
+          auto multiplicity = r.second;
+          (*x_dot_vec)(index) -= multiplicity * rxn; // -= because reactant is being removed
+        }
+
+        for (const auto & p : products)
+        {
+          auto index = p.first.index;
+          assert(index >= 0); // make sure nothing is weird with the index
+          assert(index < x_vec->size()); // make sure nothing is weird with the index
+          assert(index < x_dot_vec->size()); // make sure nothing is weird with the index
+          auto multiplicity = p.second;
+          (*x_dot_vec)(index) += multiplicity * rxn; // += because product is being added
+        }
+        int success = 0;
+        return success;
+      };
+      return fcn;
+    }
+
+    /**
+     * Returns a function for the Jacobian of the ODE. This function is not yet compatible with the SUNDIALS API.
+     * Sparse matrices in Eigen are best created from a triplet list, which must be done in one step. So instead
+     * of directly adding to the Jacobian matrix, this function appends to the provided triplet list and must be
+     * converted to a sparse matrix is a wrapper function for SUNDIALS. The Jacobian is provided to this function
+     * strictly to check indices.
+     */
+    std::function<int(N_Vector, std::vector<Eigen::Triplet<Real>> &, SUNMatrix)>
+    jacobian_function() const {
+      auto fcn = [&](N_Vector x, std::vector<Eigen::Triplet<Real>> &triplet_list, SUNMatrix Jacobian)
+      {
+        // Pull out the underlying matrix from the SUNMatrix
+        auto J = static_cast<Eigen::SparseMatrix<Real>*>(Jacobian->content);
+        // Pull out the underlying state vector from the N_Vector
+        auto x_vec = static_cast<Eigen::Matrix<Real, Eigen::Dynamic, 1>*>(x->content);
+
+        // The derivative of the rhs is nonzero with respect to each reactant.
+        for (unsigned int i=0; i<reactants.size(); ++i)
+        {
+          // Compute the derivative with respect to reactant[i]
+          Real deriv = rate;
+          for (unsigned int j=0; j<reactants.size(); ++j)
+          {
+            auto concentration = (*x_vec)(reactants[j].first.index);
+            auto coefficient = reactants[j].second;
+            if (i==j)
+            {
+              // Derivative of x^n gives n*x^{n-1}
+              deriv *= coefficient;
+              coefficient -= 1;
+              if (coefficient > 0)
+                deriv *= std::pow(concentration, coefficient);
+            }
+            else
+            {
+              deriv *= std::pow(concentration, coefficient);
+            }
+          }
+
+          // Derivative is now calculated up to plus/minus and the coefficient for each reactant/product.
+
+          // Column index in the Jacobian is based on the Species we're taking the derivative with respect to.
+          auto col = reactants[i].first.index;
+          assert(col <= x_vec->size());
+          assert(col <= J->rows());
+          assert(col <= J->cols());
+          assert(col >= 0);
+
+          // Add to the triplet list for each reactant.
+          for (const auto & chemical : reactants)
+          {
+            auto row = chemical.first.index;
+            assert(row <= x_vec->size());
+            assert(row <= J->rows());
+            assert(row <= J->cols());
+            assert(row >= 0);
+            // Reactants have negative derivatives.
+            triplet_list.push_back(Eigen::Triplet<Real>(row, col, -1.0 * chemical.second * deriv));
+          }
+
+          // Add to the triplet list for each product.
+          for (const auto & chemical : products)
+          {
+            auto row = chemical.first.index;
+            assert(row <= x_vec->size());
+            assert(row <= J->rows());
+            assert(row <= J->cols());
+            assert(row >= 0);
+            // Products have positive derivatives.
+            triplet_list.push_back(Eigen::Triplet<Real>(row, col, chemical.second * deriv));
+          }
+        }
+
+        int success = 0;
+        return success;
+      };
+      return fcn;
+    }
+
+  };
+
+
+
+  /**
+   * An object that contains instructions for the right-hand side and Jacobian of the set of
+   * ordinary differentials equations that arise from modeling a chemical reaction with the
+   * law of mass action. If using a sparse matrix, the formation of the Jacobian needs to be
+   * handled differently to achieve performance, so a partial specialization is used. This
+   * specialization is for compressed row storage.
+   */
+  template<typename Real>
+  class ChemicalReaction<Real, Eigen::SparseMatrix<Real, Eigen::RowMajor>> {
+  public:
+    /// Constructor.
+    ChemicalReaction(const std::vector< std::pair<Species, unsigned int> > reactants,
+                     const std::vector< std::pair<Species, unsigned int> > products,
+                     const Real rate)
+        : reactants(reactants),
+          products(products),
+          rate(rate)
+    {}
+
+    const std::vector< std::pair<Species, unsigned int> > reactants; /// The species and multiplicity of each reactant.
+    const std::vector< std::pair<Species, unsigned int> > products;  /// The species and multiplicity of each product.
+    const Real rate; /// The reaction rate.
+
+    /// Returns a function for the right-hand side of the ODE that is compatible with the SUNDIALS API.
+    std::function<int(Real, N_Vector, N_Vector, void*)>
+    rhs_function() const {
+      auto fcn = [&](Real time, N_Vector x, N_Vector x_dot, void * user_data)
+      {
+        // We always use N_Vectors based on the Eigen library, so use that to work with the vector.
+        auto x_vec = static_cast<Eigen::Matrix<Real, Eigen::Dynamic, 1>*>(x->content);
+        auto x_dot_vec = static_cast<Eigen::Matrix<Real, Eigen::Dynamic, 1>*>(x_dot->content);
+
+        // The right-hand side contribution from this particular reaction is based on the law of mass action.
+        //    product( reactant_i ^ multiplicity_i ) * rate
+        // Then involved reactant and product is scaled by its multiplicity.
+        Real rxn = rate;
+        for (const auto & r : reactants)
+        {
+          auto index = r.first.index;
+          assert(index >= 0); // make sure nothing is weird with the index
+          assert(index < x_vec->size()); // make sure nothing is weird with the index
+          assert(index < x_dot_vec->size()); // make sure nothing is weird with the index
+          auto exponent = r.second;
+          auto concentration = (*x_vec)(index);
+          rxn *= std::pow(concentration, exponent);
+        }
+
+        // Now distribute the reaction term to all relevant locations in the vector.
+        for (const auto & r : reactants)
+        {
+          auto index = r.first.index;
+          assert(index >= 0); // make sure nothing is weird with the index
+          assert(index < x_vec->size()); // make sure nothing is weird with the index
+          assert(index < x_dot_vec->size()); // make sure nothing is weird with the index
+          auto multiplicity = r.second;
+          (*x_dot_vec)(index) -= multiplicity * rxn; // -= because reactant is being removed
+        }
+
+        for (const auto & p : products)
+        {
+          auto index = p.first.index;
+          assert(index >= 0); // make sure nothing is weird with the index
+          assert(index < x_vec->size()); // make sure nothing is weird with the index
+          assert(index < x_dot_vec->size()); // make sure nothing is weird with the index
+          auto multiplicity = p.second;
+          (*x_dot_vec)(index) += multiplicity * rxn; // += because product is being added
+        }
+        int success = 0;
+        return success;
+      };
+      return fcn;
+    }
+
+    /**
+     * Returns a function for the Jacobian of the ODE. This function is not yet compatible with the SUNDIALS API.
+     * Sparse matrices in Eigen are best created from a triplet list, which must be done in one step. So instead
+     * of directly adding to the Jacobian matrix, this function appends to the provided triplet list and must be
+     * converted to a sparse matrix is a wrapper function for SUNDIALS. The Jacobian is provided to this function
+     * strictly to check indices.
+     */
+    std::function<int(N_Vector, std::vector<Eigen::Triplet<Real>>&, SUNMatrix)>
+    jacobian_function() const {
+      auto fcn = [&](N_Vector x, std::vector<Eigen::Triplet<Real>> &triplet_list, SUNMatrix Jacobian)
+      {
+        // Pull out the underlying matrix from the SUNMatrix
+        auto J = static_cast<Eigen::SparseMatrix<Real, Eigen::RowMajor>*>(Jacobian->content);
+        // Pull out the underlying state vector from the N_Vector
+        auto x_vec = static_cast<Eigen::Matrix<Real, Eigen::Dynamic, 1>*>(x->content);
+
+        // The derivative of the rhs is nonzero with respect to each reactant.
+        for (unsigned int i=0; i<reactants.size(); ++i)
+        {
+          // Compute the derivative with respect to reactant[i]
+          Real deriv = rate;
+          for (unsigned int j=0; j<reactants.size(); ++j)
+          {
+            auto concentration = (*x_vec)(reactants[j].first.index);
+            auto coefficient = reactants[j].second;
+            if (i==j)
+            {
+              // Derivative of x^n gives n*x^{n-1}
+              deriv *= coefficient;
+              coefficient -= 1;
+              if (coefficient > 0)
+                deriv *= std::pow(concentration, coefficient);
+            }
+            else
+            {
+              deriv *= std::pow(concentration, coefficient);
+            }
+          }
+
+          // Derivative is now calculated up to plus/minus and the coefficient for each reactant/product.
+
+          // Column index in the Jacobian is based on the Species we're taking the derivative with respect to.
+          auto col = reactants[i].first.index;
+          assert(col <= x_vec->size());
+          assert(col <= J->rows());
+          assert(col <= J->cols());
+          assert(col >= 0);
+
+          // Add to the triplet list for each reactant.
+          for (const auto & chemical : reactants)
+          {
+            auto row = chemical.first.index;
+            assert(row <= x_vec->size());
+            assert(row <= J->rows());
+            assert(row <= J->cols());
+            assert(row >= 0);
+            // Reactants have negative derivatives.
+            triplet_list.push_back(Eigen::Triplet<Real>(row, col, -1.0 * chemical.second * deriv));
+          }
+
+          // Add to the triplet list for each product.
+          for (const auto & chemical : products)
+          {
+            auto row = chemical.first.index;
+            assert(row <= x_vec->size());
+            assert(row <= J->rows());
+            assert(row <= J->cols());
+            assert(row >= 0);
+            // Products have positive derivatives.
+            triplet_list.push_back(Eigen::Triplet<Real>(row, col, chemical.second * deriv));
           }
         }
 
--- lib/src/particle_growth.h
+++ lib/src/particle_growth.h
@@ -128,5 +128,256 @@ namespace MEPBM {
       return fcn;
     }
   };
+
+
+
+  /**
+      * ParticleGrowth represents the underlying ChemicalReactions a Particle undergoes during Growth.
+      * The psuedo-elementary step represented is
+      *     sum(Reactant_i) + B ->[a*k] B + sum(Product_j)
+      * Since the Particle B encompasses many species, this is really the set of reactions
+      *     sum(Reactant_i) + B_n ->[a_n*k] B_{n+m} + sum(Product_j)
+      * where a_n is the growth kernel and m is how much the size of a particle increases during growth.
+      * Reactants and Products are not necessary, but the Particles will automatically be added to the underlying chemical reactions.
+      * This is a partial specialization for sparse matrices using compressed column storage. The formation of the
+      * Jacobian needs to be handled differently than in the dense matrix case.
+      */
+  template<typename Real>
+  class ParticleGrowth<Real, Eigen::SparseMatrix<Real>>
+  {
+  public:
+    ParticleGrowth(const Particle particle,
+                   const Real reaction_rate,
+                   const unsigned int growth_amount,
+                   const unsigned int max_particle_size,
+                   const std::function<Real(const unsigned int)> growth_kernel,
+                   const std::vector<std::pair<Species, unsigned int>> reactants,
+                   const std::vector<std::pair<Species, unsigned int>> products)
+        : particle(particle),
+          reaction_rate(reaction_rate),
+          growth_amount(growth_amount),
+          max_particle_size(max_particle_size),
+          growth_kernel(growth_kernel),
+          reactants(reactants),
+          products(products)
+    {}
+
+    const Particle particle;
+    const Real reaction_rate;
+    const unsigned int growth_amount;
+    const unsigned int max_particle_size;
+    const std::function<Real(const unsigned int)> growth_kernel;
+    const std::vector<std::pair<Species, unsigned int>> reactants, products;
+
+    /// Returns a function for the right-hand side of the ODE that is compatible with the SUNDIALS API.
+    std::function<int(Real, N_Vector, N_Vector, void*)>
+    rhs_function() const {
+      auto fcn = [&](Real time, N_Vector x, N_Vector x_dot, void * user_data)
+      {
+        int output_value;
+        // For each particle, compose the chemical reaction and then apply it.
+        for (unsigned int i=particle.index_start; i<=particle.index_end; ++i)
+        {
+          // Extract particle species
+          auto reaction_particle = particle.species(i);
+          // Form the complete list of reactants
+          auto all_reactants = reactants;
+          all_reactants.push_back({reaction_particle, 1}); // 1 particle involved as a reactant
+
+          // Form the complete list of products
+          auto all_products = products;
+          // If the created particle is a tracked size, then add it to the product list
+          if (particle.size(i) + growth_amount <= max_particle_size)
+          {
+            // Particle is indexed in increments of 1 so the growth amount of the particle also gives the index offset.
+            auto product_particle = particle.species(i+growth_amount);
+            all_products.push_back({product_particle,1}); // 1 particle involved as a product
+          }
+
+          // Form a chemical reaction
+          // Growth kernel times the base reaction rate gives the overall reaction rate
+          const Real rate = reaction_rate * growth_kernel(particle.size(i));
+          ChemicalReaction<Real, Eigen::SparseMatrix<Real>> rxn(all_reactants, all_products, rate);
+
+          // Get the right hand side function
+          auto rhs = rxn.rhs_function();
+
+          // Apply the right hand side function
+          // FIXME: should this be += instead of =?
+          output_value = rhs(time, x, x_dot, user_data);
+        }
+        return output_value;
+      };
+      return fcn;
+    }
+
+    /// Returns a function for the Jacobian of the ODE that is compatible with the SUNDIALS API.
+    std::function<int(N_Vector, std::vector<Eigen::Triplet<Real>> &, SUNMatrix)>
+    jacobian_function() const {
+      auto fcn = [&](N_Vector x, std::vector<Eigen::Triplet<Real>> &triplet_list, SUNMatrix Jacobian)
+      {
+        int output_value;
+        // For each particle, compose the chemical reaction and then apply it.
+        for (unsigned int i=particle.index_start; i<=particle.index_end; ++i)
+        {
+          // Extract particle species
+          auto reaction_particle = particle.species(i);
+          // Form the complete list of reactants
+          auto all_reactants = reactants;
+          all_reactants.push_back({reaction_particle, 1}); // 1 particle involved as a reactant
+
+          // Form the complete list of products
+          auto all_products = products;
+          // If the created particle is a tracked size, then add it to the product list
+          if (particle.size(i) + growth_amount <= max_particle_size)
+          {
+            // Particle is indexed in increments of 1 so the growth amount of the particle also gives the index offset.
+            auto product_particle = particle.species(i+growth_amount);
+            all_products.push_back({product_particle,1}); // 1 particle involved as a product
+          }
+
+          // Form a chemical reaction
+          // Growth kernel times the base reaction rate gives the overall reaction rate
+          const Real rate = reaction_rate * growth_kernel(particle.size(i));
+          ChemicalReaction<Real, Eigen::SparseMatrix<Real>> rxn(all_reactants, all_products, rate);
+
+          // Get the Jacobian function
+          auto jac = rxn.jacobian_function();
+
+          // Apply the right hand side function
+          // FIXME: should this be += instead of =?
+          output_value = jac(x, triplet_list, Jacobian);
+        }
+        return output_value;
+      };
+      return fcn;
+    }
+  };
+
+
+
+  /**
+      * ParticleGrowth represents the underlying ChemicalReactions a Particle undergoes during Growth.
+      * The psuedo-elementary step represented is
+      *     sum(Reactant_i) + B ->[a*k] B + sum(Product_j)
+      * Since the Particle B encompasses many species, this is really the set of reactions
+      *     sum(Reactant_i) + B_n ->[a_n*k] B_{n+m} + sum(Product_j)
+      * where a_n is the growth kernel and m is how much the size of a particle increases during growth.
+      * Reactants and Products are not necessary, but the Particles will automatically be added to the underlying chemical reactions.
+      * This is a partial specialization for sparse matrices using compressed row storage. The formation of the
+      * Jacobian needs to be handled differently than in the dense matrix case.
+      */
+  template<typename Real>
+  class ParticleGrowth<Real, Eigen::SparseMatrix<Real, Eigen::RowMajor>>
+  {
+  public:
+    ParticleGrowth(const Particle particle,
+                   const Real reaction_rate,
+                   const unsigned int growth_amount,
+                   const unsigned int max_particle_size,
+                   const std::function<Real(const unsigned int)> growth_kernel,
+                   const std::vector<std::pair<Species, unsigned int>> reactants,
+                   const std::vector<std::pair<Species, unsigned int>> products)
+        : particle(particle),
+          reaction_rate(reaction_rate),
+          growth_amount(growth_amount),
+          max_particle_size(max_particle_size),
+          growth_kernel(growth_kernel),
+          reactants(reactants),
+          products(products)
+    {}
+
+    const Particle particle;
+    const Real reaction_rate;
+    const unsigned int growth_amount;
+    const unsigned int max_particle_size;
+    const std::function<Real(const unsigned int)> growth_kernel;
+    const std::vector<std::pair<Species, unsigned int>> reactants, products;
+
+    /// Returns a function for the right-hand side of the ODE that is compatible with the SUNDIALS API.
+    std::function<int(Real, N_Vector, N_Vector, void*)>
+    rhs_function() const {
+      auto fcn = [&](Real time, N_Vector x, N_Vector x_dot, void * user_data)
+      {
+        int output_value;
+        // For each particle, compose the chemical reaction and then apply it.
+        for (unsigned int i=particle.index_start; i<=particle.index_end; ++i)
+        {
+          // Extract particle species
+          auto reaction_particle = particle.species(i);
+          // Form the complete list of reactants
+          auto all_reactants = reactants;
+          all_reactants.push_back({reaction_particle, 1}); // 1 particle involved as a reactant
+
+          // Form the complete list of products
+          auto all_products = products;
+          // If the created particle is a tracked size, then add it to the product list
+          if (particle.size(i) + growth_amount <= max_particle_size)
+          {
+            // Particle is indexed in increments of 1 so the growth amount of the particle also gives the index offset.
+            // FIXME: use index() function to be more clear?
+            auto product_particle = particle.species(i+growth_amount);
+            all_products.push_back({product_particle,1}); // 1 particle involved as a product
+          }
+
+          // Form a chemical reaction
+          // Growth kernel times the base reaction rate gives the overall reaction rate
+          const Real rate = reaction_rate * growth_kernel(particle.size(i));
+          ChemicalReaction<Real, Eigen::SparseMatrix<Real, Eigen::RowMajor>> rxn(all_reactants, all_products, rate);
+
+          // Get the right hand side function
+          auto rhs = rxn.rhs_function();
+
+          // Apply the right hand side function
+          // FIXME: should this be += instead of =?
+          output_value = rhs(time, x, x_dot, user_data);
+        }
+        return output_value;
+      };
+      return fcn;
+    }
+
+    /// Returns a function for the Jacobian of the ODE that is compatible with the SUNDIALS API.
+    std::function<int(N_Vector, std::vector<Eigen::Triplet<Real>> &, SUNMatrix)>
+    jacobian_function() const {
+      auto fcn = [&](N_Vector x, std::vector<Eigen::Triplet<Real>> &triplet_list, SUNMatrix Jacobian)
+      {
+        int output_value;
+        // For each particle, compose the chemical reaction and then apply it.
+        for (unsigned int i=particle.index_start; i<=particle.index_end; ++i)
+        {
+          // Extract particle species
+          auto reaction_particle = particle.species(i);
+          // Form the complete list of reactants
+          auto all_reactants = reactants;
+          all_reactants.push_back({reaction_particle, 1}); // 1 particle involved as a reactant
+
+          // Form the complete list of products
+          auto all_products = products;
+          // If the created particle is a tracked size, then add it to the product list
+          if (particle.size(i) + growth_amount <= max_particle_size)
+          {
+            // Particle is indexed in increments of 1 so the growth amount of the particle also gives the index offset.
+            auto product_particle = particle.species(i+growth_amount);
+            all_products.push_back({product_particle,1}); // 1 particle involved as a product
+          }
+
+          // Form a chemical reaction
+          // Growth kernel times the base reaction rate gives the overall reaction rate
+          const Real rate = reaction_rate * growth_kernel(particle.size(i));
+          ChemicalReaction<Real, Eigen::SparseMatrix<Real, Eigen::RowMajor>> rxn(all_reactants, all_products, rate);
+
+          // Get the Jacobian function
+          auto jac = rxn.jacobian_function();
+
+          // Apply the right hand side function
+          // FIXME: should this be += instead of =?
+          output_value = jac(x, triplet_list, Jacobian);
+        }
+        return output_value;
+      };
+      return fcn;
+    }
+  };
 }
 #endif //MEPBM_PARTICLE_GROWTH_H
