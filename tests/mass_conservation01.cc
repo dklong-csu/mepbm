cab691bdf3badf50e645c38dcd2ef035d8e6ef19
Danny <dannylongcsu@gmail.com>
2022-01-26 10:11:20 -0700

    Test mass conservation for Ir-POM system.

--- tests/mass_conservation01.cc
+++ tests/mass_conservation01.cc
@@ -33,7 +33,8 @@ Real growth_kernel(const unsigned int size)
  *    A + B -> C + L
  *    A + C -> C + L
  */
-int rhs(Real t, N_Vector x, N_Vector x_dot, void * user_data)
+MEPBM::ChemicalReactionNetwork<Real, Matrix>
+create_mechanism()
 {
   // Parameters
   const Real kb = 6.62e3;
@@ -62,20 +63,23 @@ int rhs(Real t, N_Vector x, N_Vector x_dot, void * user_data)
   MEPBM::ParticleGrowth<Real, Matrix> small_growth(B, k2, 1, max_size, &growth_kernel, { {A,1} }, { {L,1} });
   MEPBM::ParticleGrowth<Real, Matrix> large_growth(C, k3, 1, max_size, &growth_kernel, { {A,1} }, { {L,1} });
 
-  // Get the functions
-  auto rhs_nAf = nucleationAf.rhs_function();
-  auto rhs_nAb = nucleationAb.rhs_function();
-  auto rhs_nB  = nucleationB.rhs_function();
-  auto rhs_sg  = small_growth.rhs_function();
-  auto rhs_lg  = large_growth.rhs_function();
+  MEPBM::ChemicalReactionNetwork<Real, Matrix> network({nucleationAf, nucleationAb, nucleationB},
+                                                       {small_growth, large_growth},
+                                                       {});
+  return network;
+}
 
-  // Apply all functions
+
+int rhs(Real t, N_Vector x, N_Vector x_dot, void * user_data)
+{
+  auto mech = create_mechanism();
+
+  // Get the function
+  auto rhs = mech.rhs_function();
+
+  // Apply the function
   int err = 0;
-  err = rhs_nAf(t, x, x_dot, user_data);
-  err = rhs_nAb(t, x, x_dot, user_data);
-  err = rhs_nB(t, x, x_dot, user_data);
-  err = rhs_sg(t, x, x_dot, user_data);
-  err = rhs_lg(t, x, x_dot, user_data);
+  err = rhs(t, x, x_dot, user_data);
 
   return err;
 }
@@ -84,47 +88,14 @@ int rhs(Real t, N_Vector x, N_Vector x_dot, void * user_data)
 
 int jac(Real t, N_Vector x, N_Vector x_dot, SUNMatrix J, void * user_data, N_Vector tmp1, N_Vector tmp2, N_Vector tmp3)
 {
-  // Parameters
-  const Real kb = 6.62e3;
-  const Real kf = kb * 5.e-7;
-  const Real k1 = 1.24e5;
-  const Real k2 = 2.6e5;
-  const Real k3 = 6.22e3;
-  const unsigned int M = 107;
+  auto mech = create_mechanism();
 
-  const unsigned int max_size = 2500;
+  // Get the function
+  auto jac = mech.jacobian_function();
 
-  // Form the mechanism
-  // Index the vector in the order:
-  // [A->0, Asolv->1, L->2, Particles->3-2501]
-  MEPBM::Species A(0);
-  MEPBM::Species Asolv(1);
-  MEPBM::Species L(2);
-  MEPBM::Particle B(3, M, 3);
-  MEPBM::Particle C(M+1, max_size, M+1);
-
-  // Chemical reactions
-  MEPBM::ChemicalReaction<Real, Matrix> nucleationAf({ {A,1} }, { {Asolv,1}, {L,1} },11.3*11.3*kf);
-  MEPBM::ChemicalReaction<Real, Matrix> nucleationAb({ {Asolv,1}, {L,1} }, { {A,1} }, kb);
-  auto B_nucleated = B.species(3);
-  MEPBM::ChemicalReaction<Real, Matrix> nucleationB({ {Asolv,2}, {A,1} }, { {B_nucleated,1}, {L,1} }, k1);
-  MEPBM::ParticleGrowth<Real, Matrix> small_growth(B, k2, 1, max_size, &growth_kernel, { {A,1} }, { {L,1} });
-  MEPBM::ParticleGrowth<Real, Matrix> large_growth(C, k3, 1, max_size, &growth_kernel, { {A,1} }, { {L,1} });
-
-  // Get the functions
-  auto jac_nAf = nucleationAf.jacobian_function();
-  auto jac_nAb = nucleationAb.jacobian_function();
-  auto jac_nB  = nucleationB.jacobian_function();
-  auto jac_sg  = small_growth.jacobian_function();
-  auto jac_lg  = large_growth.jacobian_function();
-
-  // Apply all functions
+  // Apply the function
   int err = 0;
-  err = jac_nAf(t, x, x_dot, J, user_data, tmp1, tmp2, tmp3);
-  err = jac_nAb(t, x, x_dot, J, user_data, tmp1, tmp2, tmp3);
-  err = jac_nB(t, x, x_dot, J, user_data, tmp1, tmp2, tmp3);
-  err = jac_sg(t, x, x_dot, J, user_data, tmp1, tmp2, tmp3);
-  err = jac_lg(t, x, x_dot, J, user_data, tmp1, tmp2, tmp3);
+  err = jac(t, x, x_dot, J, user_data, tmp1, tmp2, tmp3);
 
   return err;
 }
@@ -141,7 +112,7 @@ int main ()
 
   // State start and end times
   const Real t0 = 0;
-  const Real t1 = 0.01;
+  const Real t1 = 1;
 
   // Create the matrix template
   auto template_matrix = MEPBM::create_eigen_sunmatrix<Matrix>(2501,2501);
@@ -152,17 +123,22 @@ int main ()
   // Create the CVODE object
   MEPBM::CVODE<Real> ode_solver(ic,template_matrix, linear_solver,&rhs,&jac,t0,t1);
 
-  // Solve at t=1
-  auto solution = ode_solver.solve(t1);
-  const Vector sol = *static_cast<Vector*>(solution->content);
+  // Solutions
+  const int n_solutions = 10;
+  const Real dt = (t1-t0)/n_solutions;
+  for (unsigned int s=0; s<n_solutions; ++s)
+  {
+    auto solution = ode_solver.solve(dt + s*dt);
+    const Vector sol = *static_cast<Vector*>(solution->content);
 
-  // Multiply solution vector by number of Iridium atoms in each entry
-  Real mass = 0.0;
-  mass += sol(0);
-  mass += sol(1);
-  for (unsigned int i=3; i<sol.size(); ++i)
-    mass += sol(i) * i;
+    // Multiply solution vector by number of Iridium atoms in each entry
+    Real mass = 0.0;
+    mass += sol(0);
+    mass += sol(1);
+    for (unsigned int i=3; i<sol.size(); ++i)
+      mass += sol(i) * i;
 
-  // Compare to initial number of Ir atoms
-  std::cout << (0.0012 - mass) << std::endl;
+    // Compare to initial number of Ir atoms
+    std::cout << (0.0012 - mass) << std::endl;
+  }
 }
