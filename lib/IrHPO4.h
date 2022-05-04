54f61fc2fd33da096e3874a189ae3f9150c02b9f
Danny <dannylongcsu@gmail.com>
2022-05-04 09:44:14 -0600

    General outline for organized Ir HPO4 code

---
+++ lib/IrHPO4.h
@@ -0,0 +1,34 @@
+#ifndef MEPBM_IrHPO4_H
+#define MEPBM_IrHPO4_H
+
+
+namespace MEPBM {
+  namespace IrHPO4 {
+    /*
+     * To analyse a new nanoparticle system the following is needed
+     *
+     * 1. Definition of what the mechanisms being tested are.
+     * 2. Definition of how particle growth/agglomeration reaction rates are modeled.
+     * 3. Definition of how the discrepancy between data and simulation is quantified.
+     * 4. Definition of what the starting conditions of the chemical reaction is.
+     */
+
+    /********************************************************************************
+     * Experimental design
+     ********************************************************************************/
+
+    /********************************************************************************
+     * Mechanism definitions
+     ********************************************************************************/
+
+    /********************************************************************************
+     * Growth/Agglomeration functions
+     ********************************************************************************/
+
+    /********************************************************************************
+     * Discrepancy/Log likelihood models
+     ********************************************************************************/
+  }
+}
+
+#endif //MEPBM_IrHPO4_H
