be8f6142330920cc4b750f80dff190abf6675d06
Danny <dannylongcsu@gmail.com>
2022-05-08 09:03:00 -0600

    Test when distribution has a zero.

---
+++ tests/kl_div02.cc
@@ -0,0 +1,14 @@
+#include <src/kl_divergence.h>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+
+
+int main () {
+  const std::vector<double> P = {0, .55, .45};
+  const std::vector<double> Q = {.15, .45, .4};
+
+  double kl = MEPBM::kl_divergence< double, std::vector<double> >(P, Q);
+
+  std::cout << std::setprecision(20) << kl << std::endl;
+}
