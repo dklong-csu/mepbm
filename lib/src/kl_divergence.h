0cfe2e61aa94236884380ad17464929dbd250155
Danny <dannylongcsu@gmail.com>
2022-05-08 08:56:10 -0600

    Function to calculate Kullback-Leibler divergence

---
+++ lib/src/kl_divergence.h
@@ -0,0 +1,45 @@
+#ifndef MEPBM_KL_DIVERGENCE_H
+#define MEPBM_KL_DIVERGENCE_H
+
+
+
+#include <cassert>
+#include <cmath>
+#include <numeric>
+#include <stdexcept>
+
+
+
+namespace MEPBM {
+
+  template<typename Real, typename Vector>
+  Real
+  kl_divergence(const Vector & distribution, const Vector & ref_distribution) {
+    // Make sure distributions have the same number of categories
+    assert(distribution.size() == ref_distribution.size());
+
+    // Check normalization
+    const Real distr_sum = std::accumulate(distribution.begin(), distribution.end(), 0.0);
+    if (std::abs(distr_sum - 1.0) >= 1e-8) {
+      throw std::invalid_argument("The sum of distribution elements is not equal to 1. Check the input parameter `distribution`.");
+    }
+
+    const Real ref_sum = std::accumulate(ref_distribution.begin(), ref_distribution.end(), 0.0);
+    if (std::abs(ref_sum - 1.0) >= 1e-8) {
+      throw std::invalid_argument("The sum of distribution elements is not equal to 1. Check the input parameter `ref_distribution`.");
+    }
+
+    // Compute the Kullback-Leibler divergence D(distribution || ref_distribution)
+    Real kl_div = 0.0;
+    for (unsigned int i=0; i<distribution.size(); ++i) {
+      // If distribution[i] == 0, the contribution is interpreted to be zero.
+      if (distribution[i] > 0) {
+        kl_div += distribution[i] * std::log(distribution[i] / ref_distribution[i]);
+      }
+    }
+    return kl_div;
+  }
+}
+
+
+#endif //MEPBM_KL_DIVERGENCE_H
