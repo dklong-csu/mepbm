a4d79076174a2bd7c0820470a380aed201bc56ef
Danny <dannylongcsu@gmail.com>
2022-05-19 10:02:33 -0600

    Import parameters from file

---
+++ lib/src/import_parameters.h
@@ -0,0 +1,48 @@
+#ifndef MEPBM_IMPORT_PARAMETERS_H
+#define MEPBM_IMPORT_PARAMETERS_H
+
+#include <string>
+#include <fstream>
+#include <sstream>
+
+namespace MEPBM {
+  template<typename SampleType>
+  SampleType
+  import_parameters(const std::string file_path, const unsigned int n_prm)
+  {
+    std::string line;
+    std::ifstream input_file(file_path);
+    SampleType prm(n_prm);
+    if (input_file.is_open())
+    {
+      unsigned int index = 0;
+      while ( getline(input_file,line) )
+      {
+        std::istringstream ss_in(line);
+        double prm_val = 0.0;
+        ss_in >> prm_val;
+        prm[index] = prm_val;
+        ++index;
+      }
+      input_file.close();
+      // if index < n_prm the not enough parameters were passed
+      if (index < n_prm) {
+        std::string err_msg = "Fewer than "
+                              + std::to_string(n_prm)
+                              + " were passed. Please update "
+                              + file_path
+                              + " to include "
+                              + std::to_string(n_prm)
+                              + " values arranged in a single column.\n";
+        throw std::invalid_argument(err_msg);
+      }
+    }
+    else {
+      throw std::runtime_error("Unable to open file\n");
+    }
+
+    return prm;
+  }
+}
+
+#endif //MEPBM_IMPORT_PARAMETERS_H
