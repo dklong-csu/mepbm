558deb733fc0ff7eea8ff7a25285f476f581e33a
Danny <dannylongcsu@gmail.com>
2022-05-19 12:29:11 -0600

    logistic curve function

--- tests/logistic01.cc
+++ tests/logistic01.cc
@@ -1,9 +1,12 @@
-#include "IrHPO4.h"
+#include "src/logistic_curve.h"
 #include <iostream>
 #include <iomanip>
+#include <vector>
+
+
 
 int main () {
-  MEPBM::IrHPO4::BackwardsLogisticCurve<double> curve(5,3,2);
+  MEPBM::BackwardsLogisticCurve<double> curve(5,3,2);
   std::vector<double> pts = {0, 1, 2, 3, 4, 5, 6};
   for (auto p : pts)
     std::cout << std::setprecision(20) << curve.evaluate(p) << std::endl;
