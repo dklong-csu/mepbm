8770a6444c67c1778be8c593d9fe709b1ef5d8f2
Danny Long <long@down.math.colostate.edu>
2022-10-04 12:23:29 -0600

    piecewise linear agglom kernel

--- lib/src/agglomeration_kernel.h
+++ lib/src/agglomeration_kernel.h
@@ -68,6 +68,112 @@ namespace MEPBM {
     const std::vector<unsigned int> sample_indices;
     const std::vector<unsigned int> step_locations;
   };
+
+
+
+  /**
+   * An agglomeration kernel based on a piecewise linear function combined with a modification function (e.g. Schmidt & Smirnov)
+   * @tparam Real - The floating point type.
+   * @tparam Sample - The type that contains the parameters necessary to create the kernel.
+   */
+  template<typename Real, typename Sample>
+  class PiecewiseLinearAgglomerationKernel : public BaseAgglomerationKernel<Real, Sample> {
+  public:
+    /**
+     * Constructor
+     * @param calc_surface_atoms - Function used to modify the base growth rate to account for the number of surface atoms.
+     * @param sample_indices - the indices in the sample that provide the reaction rate for each step.
+     * @param step_locations - the particle sizes corresponding to where a new step takes place.
+     */
+    PiecewiseLinearAgglomerationKernel(std::function<Real(const unsigned int)> calc_surface_atoms,
+                            const std::vector<unsigned int> sample_indices,
+                            const std::vector<unsigned int> step_locations)
+        : calc_surface_atoms(calc_surface_atoms),
+          sample_indices(sample_indices),
+          step_locations(step_locations)
+    {}
+
+
+
+    /**
+     * Returns a function that is able to calculate the reaction rate for a particle of a specified size.
+     * @param sample - the parameters relevant to creating the step function
+     * @return
+     */
+    std::function<Real(const unsigned int, const unsigned int)> get_function(const Sample & sample) const override {
+      if (step_locations.size() == 1){
+        // Global constant
+        auto result = [&](const unsigned int sizeA, const unsigned int sizeB){
+          return sample[sample_indices[0]] * calc_surface_atoms(sizeA) * calc_surface_atoms(sizeB);
+        };
+        return result;
+      }
+      else if (step_locations.size() == 2){
+        // Global linear
+        auto result = [&](const unsigned int sizeA, const unsigned int sizeB){
+          // Function is based on the difference in sizes
+          const unsigned int diff = std::abs(1.0*sizeA - 1.0*sizeB);
+
+          const double x1 = step_locations[0];
+          const double y1 = sample[sample_indices[0]];
+          const double x2 = step_locations[1];
+          const double y2 = sample[sample_indices[1]];
+
+          const double m = (y2 - y1)/(x2-x1);
+
+          return (m * (1.0*diff - x1) + y1) * calc_surface_atoms(sizeA) * calc_surface_atoms(sizeB);
+        };
+        return result;
+      }
+      else {
+        // Piecewise polynomial
+        auto result = [&](const unsigned int sizeA, const unsigned int sizeB){
+          // Function is based on the difference in sizes
+          const unsigned int diff = std::abs(1.0*sizeA - 1.0*sizeB);
+
+          // Bisection to find interval
+          unsigned int idx1 = 0;
+          unsigned int idx2 = 1;
+          if (diff >= step_locations.back()){
+            idx1 = step_locations.size()-2;
+            idx2 = step_locations.size()-1;
+          }
+          else if (diff > step_locations[0]){
+            unsigned int lo = 0;
+            unsigned int hi = step_locations.size()-1;
+            while (hi - lo > 1){
+              unsigned int mid = (hi + lo)/2;
+              if (step_locations[mid] >= diff){
+                hi = mid;
+              }
+              else {
+                lo = mid;
+              }
+            }
+            idx1 = lo;
+            idx2 = hi;
+          }
+          // Create linear function
+          const double x1 = step_locations[idx1];
+          const double y1 = sample[sample_indices[idx1]];
+          const double x2 = step_locations[idx2];
+          const double y2 = sample[sample_indices[idx2]];
+
+          const double m = (y2 - y1)/(x2-x1);
+
+          return (m * (1.0*diff - x1) + y1) * calc_surface_atoms(sizeA) * calc_surface_atoms(sizeB);
+        };
+        return result;
+      }
+    }
+
+
+
+  private:
+    const std::function<Real(const unsigned int)> calc_surface_atoms;
+    const std::vector<unsigned int> sample_indices;
+    const std::vector<unsigned int> step_locations;
+  };
 }
 
 #endif //MEPBM_AGGLOMERATION_KERNEL_H
